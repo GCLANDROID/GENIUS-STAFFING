7c0216b20ab7c03bcfa0e60aacf8588324d69b0c
GENIUS\sysdev_arpan <sahaarpan5@gmail.com>
2023-07-31 09:58:37 +0530

    commit

--- app/src/main/java/io/cordova/myapp00d753/activity/LoginActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/LoginActivity.java
@@ -45,7 +45,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.firebase.iid.FirebaseInstanceId;
+
 
 
 import org.json.JSONArray;
--- app/src/main/java/io/cordova/myapp00d753/bluedart/BlueDartAttendanceManageActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/bluedart/BlueDartAttendanceManageActivity.java
@@ -63,6 +63,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
