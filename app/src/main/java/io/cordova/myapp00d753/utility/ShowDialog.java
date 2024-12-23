9903bf0a025684c85432b35d86b3b9eb727b6254
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2024-12-23 18:51:46 +0530

    commit by Prabir 23 December 2024 -> Honasa Attendance & Weekly Off

--- app/src/main/java/io/cordova/myapp00d753/utility/ShowDialog.java
+++ app/src/main/java/io/cordova/myapp00d753/utility/ShowDialog.java
@@ -2,19 +2,47 @@ package io.cordova.myapp00d753.utility;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import io.cordova.myapp00d753.R;
+import io.cordova.myapp00d753.activity.EmployeeDashBoardActivity;
+import io.cordova.myapp00d753.activity.WeeklyOffAttendanceActivity;
 
 public class ShowDialog {
-    private static AlertDialog al1;
+    private static AlertDialog al1,successaAlertDialog;
+
+    public static void showSuccessDialog(Context context, String text,ResultListener resultListener) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context, R.style.CustomDialogNew);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View dialogView = inflater.inflate(R.layout.dialog_success, null);
+        dialogBuilder.setView(dialogView);
+        TextView tvSuccess = (TextView) dialogView.findViewById(R.id.tvSuccess);
+        tvSuccess.setText(text);
+        Button btnOk = (Button) dialogView.findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultListener.onSuccess();
+
+            }
+        });
+
+        successaAlertDialog = dialogBuilder.create();
+        successaAlertDialog.setCancelable(true);
+        Window window = successaAlertDialog.getWindow();
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        successaAlertDialog.show();
+    }
 
     public static void showErrorDialog(Context context,String text) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context, R.style.CustomDialogNew);
@@ -37,5 +65,9 @@ public class ShowDialog {
         window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
         window.setGravity(Gravity.CENTER);
         al1.show();
+    }
+
+    interface ResultListener{
+        void onSuccess();
     }
 }
