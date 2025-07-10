900ab93dd48c5c71fab0c6655a96a2244f8d8695
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2025-07-10 15:30:51 +0530

    commit by Prabir 10-July-2025

--- app/src/main/java/io/cordova/myapp00d753/activity/LoginActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/LoginActivity.java
@@ -457,7 +457,12 @@ public class LoginActivity extends AppCompatActivity {
                                     ConsentFlag=obj.optString("ConsentFlag");
                                     String LeaveBalanceView=obj.optString("LeaveBalanceView");
                                     AppData.LEAVE_BALANCE_VIEW_FLAG=LeaveBalanceView;
-
+                                    String UAN_Active = obj.optString("UAN_Active");
+                                    Log.e("UAN_Active", "UAN_Active: "+UAN_Active );
+                                    pref.saveUAN_Active(UAN_Active);
+                                    String UAN_Mandatory = obj.optString("UAN_Mandatory");
+                                    Log.e("UAN_Active", "UAN_Mandatory: "+UAN_Mandatory );
+                                    pref.saveUAN_Mandatory(UAN_Mandatory);
                                 }
                             } catch (JSONException e) {
                                 e.printStackTrace();
