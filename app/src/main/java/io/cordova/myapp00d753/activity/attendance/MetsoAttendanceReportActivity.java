e25493f9595f3c3bdadd7b647057c9dae9c33af3
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2025-11-04 11:44:47 +0530

    commit by Prabir 3-Novembor-2025

--- app/src/main/java/io/cordova/myapp00d753/activity/attendance/MetsoAttendanceReportActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/attendance/MetsoAttendanceReportActivity.java
@@ -49,7 +49,7 @@ public class MetsoAttendanceReportActivity extends AppCompatActivity {
     LinearLayout llLoder,llMain,llNodata,llAgain,llSearch;
     ImageView imgBack,imgHome,imgAgain,imgSearch;
     ProgressBar progressBar;
-    ArrayList<AttendanceModule> attendabceInfiList;
+    ArrayList<AttendanceModule> attendabceInfiList = new ArrayList<>();
     MetsoAttendanceReportAdapter metsoAttendanceReportAdapter;
     RecyclerView rvAttendanceReport;
     TextView tvYear,tvMonth;
@@ -245,7 +245,7 @@ public class MetsoAttendanceReportActivity extends AppCompatActivity {
 
                         Log.d("responseAttendance", response);
 
-                        // attendabceInfiList.clear();
+                        attendabceInfiList.clear();
 
                         try {
                             JSONObject job1 = new JSONObject(response);
@@ -256,7 +256,7 @@ public class MetsoAttendanceReportActivity extends AppCompatActivity {
                             if (responseStatus){
                                 // Toast.makeText(getApplicationContext(),responseText,Toast.LENGTH_LONG).show();
                                 JSONArray responseData=job1.optJSONArray("responseData");
-                                attendabceInfiList = new ArrayList<>();
+
                                 for (int i = 0; i < responseData.length(); i++){
                                     JSONObject obj=responseData.getJSONObject(i);
                                     String AttendanceDate=obj.optString("AttendanceDate");
