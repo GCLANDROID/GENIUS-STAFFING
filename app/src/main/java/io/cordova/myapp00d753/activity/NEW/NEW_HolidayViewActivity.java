644528d149d0c486441315375d30a83391029410
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2026-01-05 17:54:58 +0530

    commit by Prabir 5-January-2026

--- app/src/main/java/io/cordova/myapp00d753/activity/NEW/NEW_HolidayViewActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/NEW/NEW_HolidayViewActivity.java
@@ -50,7 +50,7 @@ public class NEW_HolidayViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_holiday_view);
+        setContentView(R.layout.activity_new_holiday_marking);
         initView();
         btnClick();
         loadNormalHolidayView();
