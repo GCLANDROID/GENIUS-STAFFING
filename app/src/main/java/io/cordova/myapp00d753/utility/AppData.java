fba084af14b287d0b5ba65ced7c1e12018a494d8
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2026-01-14 12:28:29 +0530

    commit by Prabir 14-January-2026

--- app/src/main/java/io/cordova/myapp00d753/utility/AppData.java
+++ app/src/main/java/io/cordova/myapp00d753/utility/AppData.java
@@ -103,15 +103,16 @@ public class AppData {
     public static String MAINTAINCEBREAK ="https://gsppi.geniusconsultant.com/GenESS/api/CheckMaintenanceBreak";
 
     public static String LAMS_GetSubServiceMenu =newv2url+"LAMS/GetSubServiceMenu";
+    //public static String LAMS_SaveAttendance ="http://171.16.1.33/GSPPI_API_V2/api/Attendance/LAMS_SaveAttendance";
     public static String LAMS_SaveAttendance =newv2url+"Attendance/LAMS_SaveAttendance";
     public static String LAMS_SaveHolidayWO =newv2url+"SKF/LAMS_SaveHolidayWO";
-    public static String LAMS_SaveAttendanceRegularization ="http://171.16.1.33/GSPPI_API_V2/api/Attendance/LAMS_SaveAttendanceRegularization";
-    public static String LAMS_DropDownData ="http://171.16.1.33/GSPPI_API_V2/api/Attendance/LAMS_DropDownData";
-    public static String LAMS_AllApplicationView ="http://171.16.1.33/GSPPI_API_V2/api/Attendance/LAMS_AllApplicationView";
-    public static String LAMS_Save_OD_CO_WFH ="http://171.16.1.33/GSPPI_API_V2/api/LAMS_EmployeeOTandODAdjustment/SaveODandOTapplication";
-    public static String LAMS_AttnRegularisationDate ="http://171.16.1.33/GSPPI_API_V2/api/Attendance/LAMS_AttnRegularisationDate";
-    public static String LAMS_DailyAttendanceReportEmp ="http://171.16.1.33/GSPPI_API_V2/api/Attendance/LAMS_DailyAttendanceReportEmp";
-    public static String LAMS_ViewHolidayList ="http://171.16.1.33/GSPPI_API_V2/api/Attendance/LAMS_ViewHolidayList";
+    public static String LAMS_SaveAttendanceRegularization =newv2url+"Attendance/LAMS_SaveAttendanceRegularization";
+    public static String LAMS_DropDownData =newv2url+"Attendance/LAMS_DropDownData";
+    public static String LAMS_AllApplicationView =newv2url+"Attendance/LAMS_AllApplicationView";
+    public static String LAMS_Save_OD_CO_WFH =newv2url+"LAMS_EmployeeOTandODAdjustment/SaveODandOTapplication";
+    public static String LAMS_AttnRegularisationDate =newv2url+"Attendance/LAMS_AttnRegularisationDate";
+    public static String LAMS_DailyAttendanceReportEmp =newv2url+"Attendance/LAMS_DailyAttendanceReportEmp";
+    public static String LAMS_ViewHolidayList =newv2url+"Attendance/LAMS_ViewHolidayList";
 
     public static String FORM_16_VIEW = newv2url+"TdsForms/ViewForm16";
     public static String FORM_16_PAYMENT = newv2url+"TdsForms/F16PaymentInitialize";
