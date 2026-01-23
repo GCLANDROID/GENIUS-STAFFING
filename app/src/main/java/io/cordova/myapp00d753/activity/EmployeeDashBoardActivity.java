cb65e5e989fe1c7881c34578b9b5a328457d2031
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2026-01-23 12:14:37 +0530

    commit by Prabir 23-January-2026

--- app/src/main/java/io/cordova/myapp00d753/activity/EmployeeDashBoardActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/EmployeeDashBoardActivity.java
@@ -701,7 +701,7 @@ public class  EmployeeDashBoardActivity extends AppCompatActivity {
                                     MenuItemModel itemModel=new MenuItemModel(MenuItem,MenuID);
                                     itemList.add(itemModel);
                                 }
-                                itemList.add(9,new MenuItemModel("Income Tax","2100"));
+                                itemList.add(4,new MenuItemModel("Income Tax","2100"));
 
 
 
