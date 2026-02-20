a8c1f6bba5b4526bee0caf7b10d1cdfca2c36bbd
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2026-02-20 17:34:09 +0530

    commit by Prabir 20-February-2026

--- app/src/main/java/io/cordova/myapp00d753/activity/MPIN_SetupActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/MPIN_SetupActivity.java
@@ -1,10 +1,12 @@
 package io.cordova.myapp00d753.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,15 +21,18 @@ import com.otpview.OTPListener;
 import com.otpview.OTPTextView;
 
 import io.cordova.myapp00d753.R;
+import io.cordova.myapp00d753.utility.ShowDialog;
 
 public class MPIN_SetupActivity extends AppCompatActivity {
     private static final String TAG = "MPIN_SetupActivity";
-    OTPTextView otpTextView;
+    OTPTextView otpTextView,otpViewConfirm;
     TextView tv1,tv2,tv3,tv4,tv5,tv6,tv7,tv8,tv9,tv0;
     LinearLayout llBackspace;
-    String MPIN="",confirm_mpin="";
+    String MPIN="",CONFIRM_MPIN="";
     TextView tvMpinText;
     ImageView imgMpin;
+    LinearLayout llEnterNewMPIN,llConfirmMPIN;
+    Button btnMPIN;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +49,7 @@ public class MPIN_SetupActivity extends AppCompatActivity {
                 .load(R.drawable.finger_tap_keypad)
                 .into(imgMpin);
         otpTextView = findViewById(R.id.otp_view);
+        otpViewConfirm = findViewById(R.id.otp_view_confirm);
         tv1 = findViewById(R.id.tv1);
         tv2 = findViewById(R.id.tv2);
         tv3 = findViewById(R.id.tv3);
@@ -56,6 +62,9 @@ public class MPIN_SetupActivity extends AppCompatActivity {
         tv0 = findViewById(R.id.tv0);
         tvMpinText = findViewById(R.id.tvMpinText);
         llBackspace = findViewById(R.id.llBackspace);
+        llEnterNewMPIN = findViewById(R.id.llEnterNewMPIN);
+        llConfirmMPIN = findViewById(R.id.llConfirmMPIN);
+        btnMPIN = findViewById(R.id.btnMPIN);
         otpTextView.setOtpListener(new OTPListener() {
             @Override
             public void onInteractionListener() {
@@ -65,7 +74,7 @@ public class MPIN_SetupActivity extends AppCompatActivity {
             @Override
             public void onOTPComplete(@NonNull String complete_otp) {
                 //Toast.makeText(MPIN_SetupActivity.this, complete_otp, Toast.LENGTH_SHORT).show();
-                if (MPIN.isEmpty()){
+                /*if (MPIN.isEmpty()){
                     MPIN =  complete_otp;
                     if (MPIN.length() == 4){
                         otpTextView.setOTP("");
@@ -82,6 +91,26 @@ public class MPIN_SetupActivity extends AppCompatActivity {
                         otpTextView.showError();
                         Toast.makeText(MPIN_SetupActivity.this, "Your m-pin has been setup successfully", Toast.LENGTH_SHORT).show();
                     }
+                }*/
+                MPIN = complete_otp;
+                if(!MPIN.isEmpty() && MPIN.length() == 4){
+                    llEnterNewMPIN.setBackgroundResource(0);
+                    llConfirmMPIN.setBackgroundResource(R.drawable.border_background_2);
+                }
+            }
+        });
+
+        otpViewConfirm.setOtpListener(new OTPListener() {
+            @Override
+            public void onInteractionListener() {
+
+            }
+
+            @Override
+            public void onOTPComplete(@NonNull String confirm_mpin) {
+                CONFIRM_MPIN = confirm_mpin;
+                if(!MPIN.isEmpty() && !confirm_mpin.isEmpty()){
+                    llConfirmMPIN.setBackgroundResource(0);
                 }
             }
         });
@@ -91,86 +120,165 @@ public class MPIN_SetupActivity extends AppCompatActivity {
         tv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"1";
-                otpTextView.setOTP(otp);
+
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"1";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"1";
+                    otpTextView.setOTP(otp);
+                }
+
             }
         });
         tv2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"2";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"2";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"2";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         tv3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"3";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"3";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"3";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         tv4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"4";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"4";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"4";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         tv5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"5";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"5";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"5";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         tv6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"6";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"6";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"6";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         tv7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"7";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"7";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"7";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         tv8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"8";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"8";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"8";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         tv9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"9";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"9";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"9";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         tv0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String otp=otpTextView.getOtp()+"0";
-                otpTextView.setOTP(otp);
+                if (!MPIN.isEmpty() && MPIN.length() == 4){
+                    String otp=otpViewConfirm.getOtp()+"0";
+                    otpViewConfirm.setOTP(otp);
+                } else {
+                    String otp=otpTextView.getOtp()+"0";
+                    otpTextView.setOTP(otp);
+                }
             }
         });
         llBackspace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!otpTextView.getOtp().isEmpty()){
-                    clearOTP(otpTextView.getOtp());
+                if (!otpViewConfirm.getOtp().isEmpty()){
+                    llEnterNewMPIN.setBackgroundResource(0);
+                    llConfirmMPIN.setBackgroundResource(R.drawable.border_background_2);
+                    clearOTP(otpViewConfirm.getOtp(),otpViewConfirm);
+                } else if (!otpTextView.getOtp().isEmpty()){
+                    llEnterNewMPIN.setBackgroundResource(R.drawable.border_background_2);
+                    llConfirmMPIN.setBackgroundResource(0);
+                    clearOTP(otpTextView.getOtp(),otpTextView);
+                }
+                MPIN = otpTextView.getOtp();
+                CONFIRM_MPIN = otpViewConfirm.getOtp();
+            }
+        });
+        btnMPIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, "onClick: MPIN: "+MPIN+" CONFIRM_MPIN: "+CONFIRM_MPIN);
+                if (MPIN.equals(CONFIRM_MPIN)){
+                    ShowDialog.showSuccessDialog(MPIN_SetupActivity.this, "MPIN match successfully", new ShowDialog.ResultListener() {
+                        @Override
+                        public void onSuccess() {
+                            Intent intent = new Intent(MPIN_SetupActivity.this, NewLoginTwoActivity.class);
+                            intent.putExtra("from","MPIN_setup");
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+                    });
+                } else {
+                    ShowDialog.showErrorDialog(MPIN_SetupActivity.this,"MPIN not match");
                 }
             }
         });
     }
 
-    private void clearOTP(String otp) {
+    private void clearOTP(String otp,OTPTextView clearOtpViewConfirm) {
         String part = otp.substring(0, otp.length()-1);
         Log.e(TAG, "clearOTP: "+part);
-        otpTextView.setOTP(part);
+        clearOtpViewConfirm.setOTP(part);
     }
 }
