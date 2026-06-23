b7cda2b9ef26fa7f53db9345332a71f8af527fd8
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2026-06-23 16:41:46 +0530

    commit by Prabir - holiday

--- app/src/main/java/io/cordova/myapp00d753/activity/FMSNewClaimActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/FMSNewClaimActivity.java
@@ -326,7 +326,7 @@ public class FMSNewClaimActivity extends AppCompatActivity {
         imgHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FMSNewClaimActivity.this, EmployeeDashBoardActivity.class);
+                Intent intent = new Intent(FMSNewClaimActivity.this, NewUserDashboardActivity.class);
                 startActivity(intent);
                 finish();
             }
