76a7cdc17984a3bd25da5b60ea9c7bcd4eb28ecb
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2026-06-24 16:37:37 +0530

    commit by Prabir - Attendance Manage

--- app/src/main/java/io/cordova/myapp00d753/activity/NEW/NEW_AttendanceRegularizationActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/NEW/NEW_AttendanceRegularizationActivity.java
@@ -60,6 +60,7 @@ import io.cordova.myapp00d753.module.SpineerItemModel;
 import io.cordova.myapp00d753.utility.AppData;
 import io.cordova.myapp00d753.utility.Pref;
 import io.cordova.myapp00d753.utility.RequiredListClass;
+import io.cordova.myapp00d753.utility.ShowDialog;
 import io.cordova.myapp00d753.utility.Util;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -453,7 +454,16 @@ public class NEW_AttendanceRegularizationActivity extends AppCompatActivity impl
                                     Log.e(TAG, "FinalStatus: "+FinalStatus);
                                     Log.e(TAG, "AlreadyRequestCount: "+AlreadyRequestCount);
                                     Log.e(TAG, "ExceedRequestLimit: "+ExceedRequestLimit);
-                                    successAlert();
+                                    //successAlert();
+                                    ShowDialog.showSuccessDialog(NEW_AttendanceRegularizationActivity.this, "Your backlog attendance was saved successfully", new ShowDialog.ResultListener() {
+                                        @Override
+                                        public void onSuccess() {
+                                            if (imgLike.getVisibility() == View.VISIBLE) {
+                                                imgLike.setVisibility(View.GONE);
+                                            }
+                                            btnShow.performClick();
+                                        }
+                                    });
                                 } else {
                                     blockLogList.clear();
                                     btnSubmit.setVisibility(View.VISIBLE);
