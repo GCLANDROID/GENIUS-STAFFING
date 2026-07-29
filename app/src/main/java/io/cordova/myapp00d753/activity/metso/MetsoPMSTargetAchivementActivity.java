643c531354f7972e3300a00ac2f3e76f69ccec70
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2026-07-29 11:11:13 +0530

    commit by Prabir - Metso PMS

--- app/src/main/java/io/cordova/myapp00d753/activity/metso/MetsoPMSTargetAchivementActivity.java
+++ app/src/main/java/io/cordova/myapp00d753/activity/metso/MetsoPMSTargetAchivementActivity.java
@@ -538,8 +538,9 @@ public class MetsoPMSTargetAchivementActivity extends AppCompatActivity {
         dialogLocationPopUp = new Dialog(MetsoPMSTargetAchivementActivity.this);
         dialogLocationPopUp.setContentView(R.layout.metso_att_location_selection_dialog);
         dialogLocationPopUp.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialogLocationPopUp.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-
+        dialogLocationPopUp .getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        TextView txtPopupHeadline = dialogLocationPopUp.findViewById(R.id.txtPopupHeadline);
+        txtPopupHeadline.setText("Select Approver Details");
         ImageView imgCancel = dialogLocationPopUp.findViewById(R.id.imgCancel);
         imgCancel.setVisibility(View.GONE);
         TextView txtSelectLocation = dialogLocationPopUp.findViewById(R.id.txtSelectLocation);
